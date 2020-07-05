56fda6f3ce88e9bcbb89640ab8f18b87d639b6b8
rhannequin <remy.hannequin@gmail.com>
2020-07-05 23:47:03 +0200

    Add class method to Constellation to retrieve easily

--- app/models/constellation.rb
+++ app/models/constellation.rb
@@ -2,4 +2,8 @@
 
 class Constellation < ApplicationRecord
   has_many :stars, dependent: :destroy
+
+  def self.abbr(abridged_name)
+    find_by!(abbreviation: abridged_name)
+  end
 end
---
+++ spec/models/constellation_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+require "csv"
+
+describe Constellation, type: :model do
+  describe "::abbr" do
+    let!(:constellation) { FactoryBot.create(:constellation) }
+
+    it "returns a constellation based on its abbreviation" do
+      expect(described_class.abbr(constellation.abbreviation)).to(
+        eq(constellation)
+      )
+    end
+
+    context "when the constellation does not exist" do
+      it "raises" do
+        expect { described_class.abbr("WRONG") }.to(
+          raise_error(ActiveRecord::RecordNotFound)
+        )
+      end
+    end
+  end
+end
